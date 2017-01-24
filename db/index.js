b983d477e4e859ed574de3c47d4b847c5a1538ee
Tenzin Sonam <tenso2006@gmail.com>
2017-01-23 17:26:43 -0800

    able to connect db to server and render static file

---
+++ db/index.js
@@ -0,0 +1,37 @@
+var mongoose  = require('mongoose');
+
+mongoose.connect('mongodb://localhost/puppy');
+
+var db = mongoose.connection;
+
+var puppySchema = mongoose.Schema({
+  userName: String,
+  email: String,
+  image: String,
+  description: String
+});
+
+var Puppies = mongoose.model('Puppies', puppySchema);
+
+
+var newPuppy = new Puppies({
+  userName: 'Bolo',
+  email: 'bplo@gmail.com',
+  image: 'bolo image',
+  description: 'bolo is new puppy in town'
+});
+
+newPuppy.save(function (err, newPuppy) {
+  if (err) return console.error(err);
+  console.log('newPuppy is :', newPuppy);
+});
+
+db.on('error', console.error.bind(console, 'connection error for mongoose puppy db'));
+
+db.once('open', function () {
+
+  console.log('connected to puppy db');
+});
+
+module.exports = db;
+
--- server.js
+++ server.js
@@ -1,15 +1,57 @@
+var db = require('./db/index.js');
+
 var express = require('express');
 
 var app = express();
+var bodyParser = require('body-parser');
+
+//get all data/stuff of the body in POST parameters
+app.use(bodyParser.json());
 
 
-//var port = process.env.port || 4000;
+app.use(bodyParser.urlencoded({extended: false}));
+
+//use and serve from client as the default static page
+app.use(express.static(__dirname + '/client'));
 
 
+
+/* ---------Routes -----------------*/
 app.get('/home', function (req, res) {
-  res.send('hello all there');
+  res.redirect('/');
+  //res.sendFile('./client/index.html');
+  //res.send('hello all there');
 });
 
+app.get('/adopt', function (req, res) {
+  db.Puppies.find(req.query, function (err, puppies){
+    if (err) {
+      return console.error('error in finding puppies in db table', err);
+    } else {
+      res.status(200).send(puppies);
+    }
+  });
+});
+
+app.post('/exchange', function (req, res) {
+  db.Puppies.save(function (err, puppies){
+    if (err) {
+      return console.error('error in posting puppies in db table', err);
+    } else {
+      console.log('req.body inside post is :', req.body);
+      var newPuppy = new Puppies(req.body);
+      res.status(201).send(newPuppy);
+    }
+  });
+});
+
+app.get('*', function (req, res) {
+  res.sendFile('./client/index.html');
+});
+
+
+var port = process.env.PORT || 4000;
+
 app.listen(4000, function () {
-  console.log('Express app listening on port:', 4000);
+  console.log('Express app listening on port:', port);
 });
